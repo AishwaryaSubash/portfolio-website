328efce57390b4dc127164a7e0dc28ceab6a3497
AishwaryaSubash <riya129ash@gmail.com>
2024-03-15 23:30:36 +0530

    Projects pg fixed

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -31,17 +31,47 @@ const Contact = () => {
                   </p>
                   <p className="max-md:text-sm">{project.description}</p>
                 </div>
-                <div className="invisible group-hover:visible flex flex-wrap gap-3 m-4 max-md:text-xs max-md:m-2">
-                  {project.techStack.map((tech, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="bg-lightHighlight text-lightBg dark:bg-darkHighlight dark:text-darkBg w-max px-2 py-1 rounded-lg"
-                      >
-                        <p>{tech}</p>
-                      </div>
-                    );
-                  })}
+                <div className=" group-hover:visible w-full flex justify-between items-end">
+                  <div className="flex flex-wrap gap-3 m-4 max-md:text-xs max-md:m-2">
+                    {project.techStack.map((tech, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="bg-lightHighlight w-fit h-fit text-lightBg dark:bg-darkHighlight dark:text-darkBg px-2 py-1 rounded-lg"
+                        >
+                          <p>{tech}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="flex flex-col m-2 text-black">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4.44c-.32-.07-.33-.68-.33-.89l.01-2.47c0-.84-.29-1.39-.61-1.67c2.01-.22 4.11-.97 4.11-4.44c0-.98-.35-1.79-.92-2.42c.09-.22.4-1.14-.09-2.38c0 0-.76-.23-2.48.93c-.72-.2-1.48-.3-2.25-.31c-.76.01-1.54.11-2.25.31c-1.72-1.16-2.48-.93-2.48-.93c-.49 1.24-.18 2.16-.09 2.38c-.57.63-.92 1.44-.92 2.42c0 3.47 2.1 4.22 4.1 4.47c-.26.2-.49.6-.57 1.18c-.52.23-1.82.63-2.62-.75c0 0-.48-.86-1.38-.93c0 0-.88 0-.06.55c0 0 .59.28 1 1.32c0 0 .52 1.75 3.03 1.21l.01 1.53c0 .21-.02.82-.34.89H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="1.5"
+                        d="M10 3.007c-2.946.032-4.59.219-5.682 1.311C3 5.636 3 7.758 3 12c0 4.243 0 6.364 1.318 7.682C5.636 21 7.758 21 12 21c4.243 0 6.364 0 7.682-1.318c1.061-1.061 1.268-2.643 1.308-5.434M21 3h-6.75M21 3v6.75M21 3l-8.25 8.25"
+                      />
+                    </svg>
+                  </div>
                 </div>
               </div>
             );
