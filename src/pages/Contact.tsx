0fa91fa44af6acd89cd2b900676dbd13ddbb3535
AishwaryaSubash <riya129ash@gmail.com>
2024-08-08 23:34:49 +0530

    mobile check - contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,12 +11,12 @@ const Contact = () => {
       <div className="h-full w-9/12 flex items-center justify-center max-lg:flex-col max-lg:items-center max-lg:gap-8 max-lg:w-11/12">
         <div className="h-full flex flex-col justify-between items-start w-9/12 max-lg:items-center max-lg:gap-6 max-lg:mb-6 max-lg:w-full">
           {/* max-md:text-xl max-sm:text-lg max-sm:flex-col max-sm:text-center max-sm:gap-0 max-sm:leading-tight */}
-          <div className="flex flex-col gap-4 font-headingFont text-5xl font-bold items-start mb-4 text-lightHighlight dark:text-darkText max-lg:flex-col max-lg:items-center max-lg:justify-center max-lg:w-full max-tablet:flex-col max-tablet:items-center max-md:text-4xl max-lg:mb-0 max-md:gap-2">
+          <div className="flex flex-col gap-4 font-headingFont text-5xl font-bold items-start mb-4 text-lightHighlight dark:text-darkText max-lg:flex-col max-lg:items-center max-lg:justify-center max-lg:w-full max-tablet:flex-col max-tablet:items-center max-md:text-4xl max-lg:mb-0 max-md:gap-2 max-sm:text-3xl">
             <p>Happy to connect.</p>
             <p>Feel free to reach out!</p>
           </div>
           <hr className="h-1 w-10/12 border-2 mt-2 mb-4 bg-black border-black rounded-full dark:bg-white dark:border-white max-lg:my-0 max-lg:w-9/12" />
-          <p className="w-10/12 font-textFont text-xl leading-relaxed py-6 max-lg:w-11/12 max-lg:text-justify max-lg:py-0">
+          <p className="w-10/12 font-textFont text-xl leading-relaxed py-6 max-lg:w-11/12 max-lg:text-justify max-lg:py-0 max-md:text-lg max-md:text-center max-sm:text-sm">
             As a full-stack developer, I'm passionate about crafting
             user-centric web applications. But my enthusiasm for collaboration
             goes beyond just projects. I believe in the power of exchanging
@@ -57,7 +57,7 @@ const Contact = () => {
                     >
                       <div
                         dangerouslySetInnerHTML={{ __html: item.icon }}
-                        className="w-10 h-10 cursor-pointer max-sm:w-12 max-sm:h-12 dark:text-darkHighlight dark:hover:text-darkHighlightOpacity"
+                        className="w-10 h-10 cursor-pointer dark:text-darkHighlight dark:hover:text-darkHighlightOpacity max-md:w-8 max-md:h-8"
                         title={item.media}
                       />
                     </a>
@@ -74,7 +74,7 @@ const Contact = () => {
             alt="Image"
             width={391}
             height={562}
-            className="transform scale-x-[-1]"
+            className="transform scale-x-[-1] max-md:w-[200px]"
           />
           {/* <img
             src="contacts-dark-image.png"
