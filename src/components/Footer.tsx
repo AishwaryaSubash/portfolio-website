692be9b51ae6de2c3e9e16f87fe13bbd8e083720
Aishwarya <92903869+AishwaryaSubash@users.noreply.github.com>
2026-01-25 17:44:31 +0530

    Update footer copyright text format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="py-4 flex flex-col gap-2 justify-center items-center bg-lightBg text-lightHighlight dark:bg-darkBg dark:text-darkText">
       <p className="font-textFont text-lightText dark:text-darkText max-sm:text-sm">
-        Aishwarya Subash &#169; {currYear}
+        &#169; {currYear} Aishwarya Subash. All rights reserved.
       </p>
       <div className="flex items-center justify-center gap-6 text-lightHighlight dark:text-darkHighlight max-lg:justify-evenly">
         {contactDetails.map((item, index) => {
